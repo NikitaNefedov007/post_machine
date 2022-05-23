8c1ae415a3dad104662e182e496eacb3a0e09916
An-Sk <90412931+An-Sk@users.noreply.github.com>
2022-05-23 16:20:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int tape_size;
+    char tape[tape_size];
+    int start_poz;
+    int start_rule;
+    vector<vector<vector<char>>> rules = {{}};
+    //char rules[id_rules][alph][3];
+
+
+    return 0;
+}
